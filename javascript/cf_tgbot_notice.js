35b2c8aafd4aed76b2ef10740cf6f0201edd9237
小米轰炸姬 <xiaomihongzhaji@gmail.com>
2024-01-12 16:32:29 +0800

    cloudflare tgbot 通知机器人

---
+++ javascript/cf_tgbot_notice.js
@@ -0,0 +1,41 @@
+const chat_id_dict = {
+    "group": -10011111111
+}
+const default_chat_id = "-10011111111";
+const token = "botxxxxxxx";
+
+export default {
+    async fetch(request, env, ctx){
+        let url = request.url;
+        console.log("url", url);
+        url = url.substring(url.indexOf("//") + 2);
+        url = url.substring(url.indexOf("/") + 1);
+        const key = url.substring(0, url.indexOf("/"));
+        console.log("key", key);
+        let text = url.substring(url.indexOf("/") + 1);
+        console.log("text", text);
+        if(!text){
+            text = "【空白消息】";
+        }
+        let chat_id = chat_id_dict[key];
+        console.log("chat_id", chat_id);
+        if(!chat_id){
+            chat_id = default_chat_id;
+            if(key){
+                text = key + "/" + text;
+            }
+        }
+        let newUrl = 'https://api.telegram.org/';
+        newUrl += token;
+        newUrl += '/sendMessage?';
+        newUrl += 'chat_id=' + chat_id;
+        newUrl += '&text=' + text;
+        console.log("newUrl", newUrl);
+        const response = await fetch(newUrl);
+        console.log("response", response);
+        if(response.ok){
+            return Response.json({msg: "发送成功"});
+        }
+        return Response.json({msg: response.statusText});
+    }
+}
